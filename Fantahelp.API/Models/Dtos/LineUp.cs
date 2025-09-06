41c47ba12345162259417097e7575601578082c9
duiliocirino <10621933@polimi.it>
2025-09-06 11:59:46 +0200

    refactor: moved DTOs for League, Player, Team, User, and Score models

--- Fantahelp.API/Models/Dtos/LineUp.cs
+++ Fantahelp.API/Models/Dtos/LineUp.cs
@@ -5,8 +5,4 @@ public class LineUp
     public int Defenders { get; set; }
     public int Midfielders { get; set; }
     public int Attackers { get; set; }
-    public static string ToString(LineUp lineUp)
-    {
-        return $"{lineUp.Defenders}-{lineUp.Midfielders}-{lineUp.Attackers}";
-    }
 }
--- Fantahelp.API/Models/Dtos/Read/TeamReadDto.cs
+++ Fantahelp.API/Models/Dtos/Read/TeamReadDto.cs
@@ -2,5 +2,6 @@ public class TeamReadDto
 {
     public required int Id { get; set; }
     public required string Name { get; set; }
+    public required int RemainingBudget { get; set; }
     public required List<TeamPlayerReadDto> Players { get; set; }
 }
